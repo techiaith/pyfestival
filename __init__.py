ccddae01e7154dc862c65f84713418d1370a3fb0
Patrick Robertson <p.robertson@bangor.ac.uk>
2014-12-15 16:08:17 +0000

    Defnyddio subprocess yn lle os.system

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 import _festival
 import six
-import os
+import subprocess
 
 def textToWav(text):
     if not isinstance(text, six.text_type):
         text = text.decode('utf-8')
     out_file = _festival._textToWav(text)
-    return os.system("lame --quiet -V 9 %s -" % out_file)
+    cmd = "lame --quiet -V 9 %s -" % out_file
+    return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
 def sayText(text):
     if not isinstance(text, six.text_type):
