089a9aa9aa214c62e091282c27d9b0669c6f5d8a
Patrick Robertson <p.robertson@bangor.ac.uk>
2014-12-17 14:25:32 +0000

    Ehangu'r API festival

--- __init__.py
+++ __init__.py
@@ -1,20 +1,72 @@
 from . import festival
 import six
 import subprocess
+import tempfile
+import os
 
-def textToWav(text):
+def textToWav_(text):
+    """Returns a filename for a wav file created
+    with the text 'text'
+    
+    Use this method if you are concerned about removing
+    the wav file once you have used it.
+    Being given the filename, you are responsible for opening it
+    as you see fit, and removing it once done
+    """
     if not isinstance(text, six.text_type):
         text = text.decode('utf-8')
-    wav_file = festival._textToWav(text)
-    cmd = "lame --quiet -V 9 %s -" % wav_file
+    return festival._textToWav(text)
+
+def textToWav(text):
+    """Returns a file object for a wav file created with
+    the text 'text'
+    
+    Note: it is your responsibility to remove the file when
+    you are finished with it. It is automatically removed
+    when you close the file.
+    
+    This method may return None if an error occurs
+    """
+    
+    tmp_path = textToWav_(text)
+    try:
+        with open(tmp_path, 'rb') as w:
+            tmp_file = tempfile.SpooledTemporaryFile(max_size=41000, mode='wb')
+            tmp_file.write(w.read())
+            tmp_file.seek(0)
+        return tmp_file
+    finally:
+        os.remove(tmp_path)
+    
+    return None
+    
+def textToMp3(text):
+    """Returns a file-like object (pipe) for an mp3 file
+    created with the text 'text'
+    
+    Use Lame to encode the wav file to an mp3
+    """
+
+    cmd = "lame --quiet -V 9 %s -" % textToWav_(text)
     return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
 def sayText(text):
+    """Tells festival to say the text 'text'
+    
+    Only works for machines with a sound card and audio device
+    connected. May otherwise return errors
+    """
     if not isinstance(text, six.text_type):
         text = text.decode('utf-8')
     festival._sayText(text)
 
 def setStretchFactor(f):
+    """Set the stretch factor of the audio to be returned.
+    
+    f is a float between 0 and 5. A number less than 1 speeds up
+    the voice (e.g. 0.5 doubles the speed). A number >1 slows down
+    the voice (e.g. 2 halves the speed)
+    """
     try:
         f = float(f)
     except ValueError:
@@ -22,4 +74,11 @@ def setStretchFactor(f):
     festival.setStretchFactor(f)
     
 def execCommand(cmd):
+    """Execute a festival command
+    
+    Festival commands are written in lisp, and should
+    be formatted correctly.
+    
+    E.g. execCommand("(SayText \"helo\")")
+    """
     festival.execCommand(cmd)
