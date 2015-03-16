7d837eb2a5c7ae3a0e78b998009b93231ac48776
Patrick Robertson <p.robertson@bangor.ac.uk>
2015-03-16 13:27:19 +0000

    Dileu'r ffeil __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .festival import sayText, sayFile, textToWav, textToMp3, textToWavFile, textToMp3File, setStretchFactor, execCommand
-
-
-__all__ = [sayText, sayFile, textToWav, textToMp3, textToWavFile, textToMp3File, setStretchFactor, execCommand]
